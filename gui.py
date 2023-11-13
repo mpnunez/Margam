69580896ac6ef13a2a9bf7c2efdffbccb5ff2393
Marcel Nunez <mpnunez28@gmail.com>
2023-11-12 18:20:36 -0800

    GUI does not show board until game is complete

--- gui.py
+++ gui.py
@@ -4,7 +4,50 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QPixmap
 
-def window():
+from player import RandomPlayer, HumanPlayer
+from game import Game
+from player import Player
+import numpy as np
+import time
+
+class HumanGUIPlayer(Player):
+    
+    def __init__(self,name=None,gui=None):
+        super().__init__(name)
+        self.gui = gui
+    
+    def get_move_scores(self,board: np.array) -> np.array:
+        n_cols = board.shape[2]
+        invalid_input = True
+        while invalid_input:
+            new_input = input(f"Select column to drop token into [0-{n_cols-1}]\n")
+            try:
+                slot_to_drop = int(new_input)
+            except ValueError:
+                continue
+            
+            if 0 <= slot_to_drop and slot_to_drop < n_cols:
+                invalid_input = False
+                
+        scores = np.zeros(n_cols)
+        scores[slot_to_drop] = 1
+        return scores
+    
+    def get_move_scores_gui(self,board: np.array) -> np.array:
+        n_cols = board.shape[2]
+        
+        # Waiting for button press
+        slot_to_drop = None
+        while slot_to_drop is None:
+            #slot_to_drop = gui.last_button_pressed() # ??
+            return np.array([1,0,0,0,0,0,0])
+            time.sleep(0.10)
+                
+        scores = np.zeros(n_cols)
+        scores[slot_to_drop] = 1
+        return scores
+
+def window(game: Game):
     app = QApplication(sys.argv)
     win = QWidget()
     grid = QGridLayout()
@@ -17,8 +60,6 @@ def window():
            pixmap = pixmap.scaledToWidth(100)
            label = QLabel()
            label.setPixmap(pixmap)
-           #label = label.resize(pixmap.width()//2,
-           #               pixmap.height()//2)
            grid.addWidget(label,i,j)
            
     for j in range(ncols):
@@ -29,7 +70,19 @@ def window():
     win.setWindowTitle("PyQt Grid Example")
     win.setGeometry(50,50,200,200)
     win.show()
+    
+    game.play_game(show_board_each_move=True)
+    
     sys.exit(app.exec_())
 
+def main():
+    g = Game()
+    human = HumanGUIPlayer()
+    g.players = [human,RandomPlayer()]
+    
+    window(g)
+    
+    
+
 if __name__ == '__main__':
-   window()
+   main()
