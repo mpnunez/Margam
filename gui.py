d7e1dd400c104de3608151661e50bc679a64ea02
Marcel Nunez <mpnunez28@gmail.com>
2023-11-12 17:56:14 -0800

    Add drop buttons

--- gui.py
+++ gui.py
@@ -5,22 +5,31 @@ from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QPixmap
 
 def window():
-   app = QApplication(sys.argv)
-   win = QWidget()
-   grid = QGridLayout()
-	
-   for i in range(0,6):
-      for j in range(0,7):
-          pixmap = QPixmap('empty.png')
-          label = QLabel()
-          label.setPixmap(pixmap)
-          grid.addWidget(label,i,j)
-			
-   win.setLayout(grid)
-   win.setWindowTitle("PyQt Grid Example")
-   win.setGeometry(50,50,200,200)
-   win.show()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    win = QWidget()
+    grid = QGridLayout()
+    	
+    nrows = 6
+    ncols = 7
+    for i in range(nrows):
+       for j in range(ncols):
+           pixmap = QPixmap('empty.png')
+           pixmap = pixmap.scaledToWidth(100)
+           label = QLabel()
+           label.setPixmap(pixmap)
+           #label = label.resize(pixmap.width()//2,
+           #               pixmap.height()//2)
+           grid.addWidget(label,i,j)
+           
+    for j in range(ncols):
+        grid.addWidget(QPushButton("Drop"),nrows,j)
+           
+    			
+    win.setLayout(grid)
+    win.setWindowTitle("PyQt Grid Example")
+    win.setGeometry(50,50,200,200)
+    win.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
    window()
