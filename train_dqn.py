d13cbd56e3b21033a1059ac55497c750d36939f1
Marcel Nunez <marcelpnunez@gmail.com>
2024-01-25 19:21:39 -0800

    Yield transitions from a generator
    Instead of getting a batch of data from a batch of games

--- train_dqn.py
+++ train_dqn.py
@@ -7,8 +7,13 @@ from keras.models import load_model
 from collections import Counter
 from enum import Enum
 import itertools
+from collections import deque
 
 def play_match(agent,opponents,i):
+    """
+    Play a single game and return
+    transition records for the agent
+    """
     opponent_ind = (i//2)%len(opponents)    # Play each opponent twice in a row
     opponent = opponents[opponent_ind]
     agent_position = i%2
@@ -23,59 +28,48 @@ def play_match(agent,opponents,i):
     winner, records = g.play_game()
     agent_records = records[agent_position::len(g.players)]
 
-    return agent_records, agent_records[-1].reward, opponent_ind
+    return agent_records
 
-def play_matches(agent, opponents, n_games=100):
-    agent_move_records = []
-    agent_wlt_record = np.zeros([len(opponents),3],dtype=int)
-    
-    print(f"Playing {n_games} games...")
-    game_results = (play_match(agent,opponents,i) for i in tqdm(range(n_games)))
-
-    for agent_records, agent_reward, opponent_ind in game_results:
-        agent_move_records += agent_records
-        reward_to_table_ind = {1: 0, 0: 1, 0.5: 2}  # win / loss / tie
-        agent_wlt_record[opponent_ind,reward_to_table_ind[agent_reward]] += 1
+def generate_transitions(agent, opponents):
+    """
+    Infinitely yield transitions by playing
+    game episodes
+    """
+    i = 0
+    while True:
+        agent_records = play_match(agent,opponents,i)
+        for move_record in agent_records:
+            yield move_record
+        i += 1
 
     return agent_move_records, agent_wlt_record
         
 
 def main():
     
-    magnus = DQNPlayer(name="Magnus")
-    #magnus.random_weight = 0.2
-    random_bot = RandomPlayer("Random Bot")
-    #opponents = [random_bot, ColumnSpammer(name="ColumnSpammer")]
-    #opponents = [ColumnSpammer(name=f"ColumnSpammer-{i}",col_preference=i) for i in range(7)]
-    #opponents += [RandomPlayer(f"RandomBot_{i}") for i in range(7)]
-    #opponents = [ColumnSpammer(name=f"ColumnSpammer",col_preference=4)]
-    opponents = [RandomPlayer(name=f"Random Bot")]
-    self_play = False
-    percentile_keep = 0.3       # Train on this fraction of best games
-    SAVE_MODEL_EVERY_N_BATCHES = 100
-    #GAMES_PER_TRAINING_BATCH = 100
-    GAMES_PER_TRAINING_BATCH = 10
+    agent = DQNPlayer(name="Magnus")
+    opponents = [RandomPlayer(name=f"RandomBot")]
+
+    # DQN hyperparameters
+    SAVE_MODEL_EVERY_N_TRANSITIONS = 100
     N_TRAINING_BATCHES = 1000
 
-    for training_round in range(N_TRAINING_BATCHES):
+    GAMMA = 0.99
+    BATCH_SIZE = 32             
+    REPLAY_SIZE = 10000
+    LEARNING_RATE = 1e-4
+    SYNC_TARGET_NETWORK = 1000
+    REPLAY_START_SIZE = 10000
+
+    total_transitions = 0
+
+    for transition in generate_transitions(agent, opponents):
     
-        agent_move_records, win_loss_ties = play_matches(magnus, opponents, n_games=GAMES_PER_TRAINING_BATCH)
+        total_transitions += 1
+        print(transition)
+        if total_transitions > 20:
+            break
 
-        # Print table of win/loss/tie/records
-        print("Opponent\tWins\tLosses\tTies")
-        for op, row in zip(opponents,win_loss_ties):
-            print(f"{op.name}\t",end='')
-            for col in row:
-                print(f"{col}\t",end='')
-            print()
-            
-        # View distribution of agent moves
-        selected_moves = [mr.selected_move for mr in agent_move_records]
-        move_counts = [selected_moves.count(i) for i in range(7)]
-        print(move_counts)
-
-
-        return
 
 if __name__ == "__main__":
     main()
