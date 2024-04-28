6e77fb0b055741896518fc8f05ebf7a65a3b6635
Marcel Nunez <marcelpnunez@gmail.com>
2024-04-27 17:16:48 -0700

    Play against trained tic tac toe agent

--- play_game.py
+++ play_game.py
@@ -6,13 +6,19 @@ from collections import deque
 from functools import partial
 
 from connect4lib.game import TicTacToe, Connect4
-from connect4lib.agents import RandomPlayer, HumanPlayer, MiniMax
+from connect4lib.agents import RandomPlayer, HumanPlayer, MiniMax, ReinforcePlayer
+
+from keras.models import load_model
 
 import click
 
 game_classes = {
     "tictactoe": TicTacToe,
     "connect4": Connect4,
+}
+oppenents = {
+    "minimax": MiniMax(name="Maximum",max_depth=2),
+    "pg": ReinforcePlayer(name="Rein"),
 }
 
 @click.command()
@@ -21,13 +27,18 @@ game_classes = {
     case_sensitive=False),
     default="tictactoe",
     show_default=True)
-def main(game_type):
+@click.option('-o', '--opponent',
+    type=click.Choice(['minimax', 'pg'],
+    case_sensitive=False),
+    default="pg",
+    show_default=True)
+def main(game_type,opponent):
     # Enable choosing opponent with CLI
     
     # Intialize players
     agent = HumanPlayer(name="Marcel")
-    opponent = MiniMax(name="Maximus")
-    opponent.max_depth = 3
+    opponent = ReinforcePlayer(name="Rein")
+    opponent.model = load_model("reinforce-tic.keras")
     g = game_classes[game_type.lower()]()
     g.players = [opponent,agent]
     g.verbose = True
